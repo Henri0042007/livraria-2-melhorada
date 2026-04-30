490541f798998a541761214cf8a3697266a5914e
Henri0042007 <henrique.santiago.sb@gmail.com>
2026-04-30 10:48:48 -0300

    FEAT:Dump load de dados

--- core/models/autor.py
+++ core/models/autor.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class Autor(models.Model):
     nome = models.CharField(max_length=100)
-    email = models.EmailField()
+    email = models.EmailField(blank=True, null=True)
     def __str__(self):
         return self.nome
