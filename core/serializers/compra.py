a8a217663cf08e037097f454281edffbe342b183
Henri0042007 <henrique.santiago.sb@gmail.com>
2026-08-14 09:20:57 -0300

    feat: ajuste

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -4,11 +4,14 @@ from core.models import Compra, ItensCompra
 
 
 class ItensCompraSerializer(ModelSerializer):
+    titulo = CharField(source='livro.titulo', read_only=True)
+    editora = CharField(source='livro.editora.nome', read_only=True)
+    preco = DecimalField(source='livro.preco', read_only=True, max_digits=10, decimal_places=2)
+    capa = CharField(source='livro.capa.url', read_only=True)
+
     class Meta:
         model = ItensCompra
-        fields = ('livro', 'quantidade')
-        depth = 1
-
+        fields = ('id', 'titulo', 'editora', 'quantidade', 'preco', 'capa')
 
 class CompraSerializer(ModelSerializer):
     usuario = CharField(source='usuario.email', read_only=True)
