207e36de8c42e1b0e6ae6ffb91539d03781a2ea7
Henri0042007 <henrique.santiago.sb@gmail.com>
2026-08-20 11:02:12 -0300

    feat: mostrando o total do item na listagem de compras

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -1,17 +1,17 @@
-from rest_framework.serializers import CharField, ModelSerializer
+from rest_framework.serializers import CharField, DecimalField ,ModelSerializer, SerializerMethodField
 
 from core.models import Compra, ItensCompra
 
 
 class ItensCompraSerializer(ModelSerializer):
-    titulo = CharField(source='livro.titulo', read_only=True)
-    editora = CharField(source='livro.editora.nome', read_only=True)
-    preco = DecimalField(source='livro.preco', read_only=True, max_digits=10, decimal_places=2)
-    capa = CharField(source='livro.capa.url', read_only=True)
+    total = SerializerMethodField()
+
+    def get_total(self, instance):
+        return instance.livro.preco * instance.quantidade
 
     class Meta:
         model = ItensCompra
-        fields = ('id', 'titulo', 'editora', 'quantidade', 'preco', 'capa')
+        fields = ('id', 'titulo', 'editora', 'quantidade', 'preco', 'total', 'capa')
 
 class CompraSerializer(ModelSerializer):
     usuario = CharField(source='usuario.email', read_only=True)
